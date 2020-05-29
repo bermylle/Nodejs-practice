48a2225617288c3aaeac81afb84879316deedcde
bermylle <bermylle@gmail.com>
2020-05-29 18:13:14 +0800

    add

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
+// Core NodeJS module
+const path = require('path');
+// 3rd Party modules
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+// Templating Engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// Routes
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req,res,next) => {
-    res.status(404).send('<h1> 404 ERROR </h1>')
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path : 'ERROR' });
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,26 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.get('/add-product',(req, res, next) => {
-    //console.log('inside middleware')
-    res.send('<form action = "/admin/add-product" method = "POST" ><input type="text" name = "title"> <button type="submit"> Add Product </button> </form>');
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product'
+  });
 });
 
-router.post('/add-product',(req, res, next) => {
-    console.log(req.body);
-    //console.log(JSON.parse(JSON.stringify(req.body)));
-    res.redirect('/');
-}); 
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
 
-
-module.exports = router;
+exports.routes = router;
+exports.products = products;
