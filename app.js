9e04b49c24bee540c7dfd73354ece5740dc56413
bermylle <bermylle@gmail.com>
2020-06-04 18:17:58 +0800

    MONGODB

--- app.js
+++ app.js
@@ -10,54 +10,24 @@ const app = express();
 // Templating Engine
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 // Controller
 const errorController = require('./controllers/error');
 
-
-
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 app.use(errorController.get404);
 
-// Sequelize
-const sequelize = require('./util/database');
+// MongoDB connector import
+const mongoConnect = require('./util/database');
 
-// Create DB, tables, etc.
-sequelize
-.sync()
-//.sync({ force: true })
-    .then(result => {
-        return User.findByPk(1);
-    })
-    .then(user => {
-        if (!user) {
-            return User.create({
-                first_name: 'Bermylle',
-                last_name: 'Razon',
-                email: 'bermylle@gmail.com'
-            })
-        }
-        return user;
-    })
-    .then(user => {
-        
-        return user.createCart();
-    })
-    .then(cart => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-
-
-
+mongoConnect((client) => {
+    console.log(client);
+    app.listen(3000);
+});
--- util/database.js
+++ util/database.js
@@ -2,4 +2,13 @@ const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect('mongodb+srv://bermyllerazon:<>@cluster0-mlvbq.mongodb.net/<dbname>?retryWrites=true&w=majority');
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://bermyllerazon:sOlEw2ysv3wEWDbG@cluster0-mlvbq.mongodb.net/<dbname>?retryWrites=true&w=majority')
+    .then(cl => {
+        console.log('MONGODB connected');
+        callback(cl);
+    })
+    .catch(err => console.log(err));
+};
+
+module.exports = mongoConnect;
