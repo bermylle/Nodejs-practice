863cb3c6cb0129d27c9485134a2373a399c9839d
bermylle <bermylle@gmail.com>
2020-06-04 18:00:17 +0800

    MONGODB

--- app.js
+++ app.js
@@ -18,22 +18,8 @@ const shopRoutes = require('./routes/shop');
 // Controller
 const errorController = require('./controllers/error');
 
-// Product Models [Import]
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-items');
 
-app.use((req,res,next) => {
-    User.findByPk(1)
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
-})
+
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -41,19 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
-
-
-// Associations [Relations]
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, {through: OrderItem});
-
 
 // Sequelize
 const sequelize = require('./util/database');
