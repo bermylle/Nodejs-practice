82406cec12bae47eea00c09322e4da54ee7bbef4
bermylle <bermylle@gmail.com>
2020-06-04 20:22:49 +0800

    MONGODB

--- app.js
+++ app.js
@@ -27,48 +27,6 @@ app.use(errorController.get404);
 // MongoDB connector import
 const mongoConnect = require('./util/database').mongoConnect;
 
-<<<<<<< HEAD
-// Associations [Relations]
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-
-// Sequelize
-const sequelize = require('./util/database');
-
-// Create DB, tables, etc.
-sequelize
-.sync()
-    .then(result => {
-        return User.findByPk(1);
-    })
-    .then(user => {
-        if (!user) {
-            return User.create({
-                first_name: 'Bermylle',
-                last_name: 'Razon',
-                email: 'bermylle@gmail.com'
-            })
-        }
-        return user;
-    })
-    .then(user => {
-        
-        return user.createCart();
-    })
-    .then(cart => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-
-
-
-=======
 app.use((req,res,next) => {
     next();
 })
@@ -76,4 +34,3 @@ mongoConnect((client) => {
     console.log(client);
     app.listen(3000);
 });
->>>>>>> mongodb
